0ae572fc2cceba4c68df80a283caec74bc742596
Nicolas Toledo <odelotsalocin@gmail.com>
2020-11-25 09:26:41 -0300

    base de datos cargada con comentarios

--- cinema_db.sql
+++ cinema_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 19-11-2020 a las 15:53:01
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.6
+-- Tiempo de generación: 25-11-2020 a las 13:24:27
+-- Versión del servidor: 10.4.13-MariaDB
+-- Versión de PHP: 7.2.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,6 +35,18 @@ CREATE TABLE `comentarios` (
   `comentario` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `comentarios`
+--
+
+INSERT INTO `comentarios` (`id`, `id_pelicula`, `id_usuario`, `puntaje`, `comentario`) VALUES
+(2, 29, 4, 4, 'Tremenda pelicula soy Angi, saludos. xd'),
+(3, 36, 1, 2, 'Pelicula de terror chota, no se muere nadie'),
+(6, 29, 1, 3, 'comentario desde js'),
+(7, 29, 5, 2, 'Nuevo comentario desde js'),
+(8, 29, 5, 3, 'tercer intento'),
+(9, 36, 5, 1, 'Alta pelicula de miarda');
+
 -- --------------------------------------------------------
 
 --
@@ -52,7 +64,6 @@ CREATE TABLE `genero` (
 
 INSERT INTO `genero` (`id_genero`, `nombre`) VALUES
 (24, 'Comedia'),
-(25, 'Acción'),
 (26, 'Terror'),
 (28, 'Drama'),
 (29, 'Ciencia ficción');
@@ -79,9 +90,6 @@ CREATE TABLE `peliculas` (
 
 INSERT INTO `peliculas` (`id`, `titulo`, `sinopsis`, `duracion`, `id_genero`, `puntuacion`, `precio`) VALUES
 (29, 'Swiss Army Man', 'Hank (Paul Dano), un náufrago en una isla del Pacífico que está a punto de ahorcarse, ve llegar a la playa un cadáver (Daniel Radcliffe). Intenta reanimarlo, pero el cadáver lo mira sin dejar de solta', '1:40', 24, 5, 500),
-(30, ' Baby Driver', 'Baby es un chofer especializado en fugas que, enamorado, pretende dejar la vida criminal y empezar de cero con la mujer que ama. Cuando el jefe de una banda de gánsters le obliga a trabajar para él y ', '2:00', 25, 5, 300),
-(31, ' John Wick', 'Reeves interpreta a John Wick, un sicario retirado que busca venganza por la muerte del perro que le dio su esposa recientemente fallecida', '2:00', 25, 5, 300),
-(35, 'El club de los abrazos', 'Parodia del club de la pelea', '5m', 24, 3, 40),
 (36, 'Annabelle', 'Una preciosa muñeca antigua llamada Annabelle. Una noche, una secta satánica les ataca brutalmente y provocan que un ente maligno se apodere de Annabelle.', '1:40', 26, 2, 300);
 
 -- --------------------------------------------------------
@@ -105,8 +113,8 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`id`, `userName`, `email`, `password`, `superUser`) VALUES
 (1, 'User1', 'user1@mail.com', '$2y$10$.MhrrRPigd7YGHPCaT/exuRUkw3wJMfTTvl3r8wBfknTvGxRsDdkm', 1),
 (2, 'User2', 'user2@mail.com', '$2y$10$Cv4xXgWdHtaBlebtcpGKJ.IS1hwCExFzQ75Vtax42yOT4.eRAMtyS', 0),
-(3, 'User3', 'user3@mail.com', '$2y$10$ojOK66VUjk5lA4oxAKJi1e0JIGvrydONCKiV7K/X/irvAOZdgDtLO', 0),
-(4, 'Angi', 'angi@mail.com', '$2y$10$RlVOLr7UyhWQiRY/QiutveT2kk5Tz9U5pYaDSM.emVM4nWroQx.Hm', 1),
+(3, 'User3', 'user3@mail.com', '$2y$10$ojOK66VUjk5lA4oxAKJi1e0JIGvrydONCKiV7K/X/irvAOZdgDtLO', 1),
+(4, 'Angi', 'angi@mail.com', '$2y$10$RlVOLr7UyhWQiRY/QiutveT2kk5Tz9U5pYaDSM.emVM4nWroQx.Hm', 0),
 (5, 'Nico', 'nico@mail.com', '$2y$10$xz6oOS5yQbZqhjmdS8kFruEQR0hR8MHLaZOjTWGwesPkbPVL4wza2', 1),
 (7, 'User5', 'user5@mail.com', '$2y$10$sziISyAncVmjDjbSNo8/dut7Nhi7ySikZ8G1Sp5I4E6X8kuvt7lpa', 0);
 
@@ -149,19 +157,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `genero`
 --
 ALTER TABLE `genero`
-  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_genero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `peliculas`
 --
 ALTER TABLE `peliculas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
